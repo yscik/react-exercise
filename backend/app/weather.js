0bfc951b74ce2914137e4c18236285ccb72b8ca7
Peter Kiss <peter@yscik.com>
2019-10-28 19:31:28 +0100

    Move openweathermap call to backend weather service

---
+++ backend/app/weather.js
@@ -0,0 +1,36 @@
+import {formatTime, adjustDateForTimezone} from "../../src/timeFormat.js";
+import fetch from 'node-fetch';
+
+export default {
+  async getWeather({city}) {
+    const apiKey = 'c4e590cec2f4b2a3cefdcb13652c98f9';
+    const weatherInfo = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`).then(r => r.json());
+
+    return formatWeatherData(weatherInfo);
+  }
+}
+
+function formatWeatherData(w) {
+
+  const timezone = (w.timezone || 0) / 3600;
+
+  return {
+    city: {
+      name: w.name,
+      timezone: timezone
+    },
+
+    icon: w.weather[0].id,
+    description: w.weather[0].description,
+    temperature: `${w.main.temp.toFixed(0)} °C`,
+    sunrise: getFormattedTime(w.sys.sunrise),
+    sunset: getFormattedTime(w.sys.sunset),
+
+  };
+
+  function getFormattedTime(timestamp) {
+    const date = new Date(timestamp*1000);
+    adjustDateForTimezone(date, timezone);
+    return formatTime(date, {separator: ':'});
+  }
+}
--- src/store/weather.actions.js
+++ src/store/weather.actions.js
@@ -1,5 +1,4 @@
 import {apiGet} from "../api.js";
-import {adjustDateForTimezone, formatTime} from "../timeFormat.js";
 
 export function weatherLoaded(weather) {
   return {
@@ -29,12 +28,9 @@ export function loadWeatherForCity(city)
 
     try {
       const query = encodeURIComponent(city);
-      const apiKey = 'c4e590cec2f4b2a3cefdcb13652c98f9';
+      const weatherInfo = await apiGet(`/weather/${query}`);
 
-      const weatherInfo = await
-          apiGet(`//api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=metric`)
-
-      dispatch(weatherLoaded(formatWeatherData(weatherInfo)));
+      dispatch(weatherLoaded(weatherInfo));
     }
     catch(err) {
       dispatch(weatherLoadError(err))
@@ -42,31 +38,6 @@ export function loadWeatherForCity(city)
   }
 }
 
-function formatWeatherData(w) {
-
-  console.log(w)
-  const timezone = (w.timezone || 0) / 3600;
-
-  return {
-    city: {
-      name: w.name,
-      timezone: timezone
-    },
-
-    icon: w.weather[0].id,
-    description: w.weather[0].description,
-    temperature: `${w.main.temp.toFixed(0)} °C`,
-    sunrise: getFormattedTime(w.sys.sunrise),
-    sunset: getFormattedTime(w.sys.sunset),
-
-  };
-
-  function getFormattedTime(timestamp) {
-    const date = new Date(timestamp*1000);
-    adjustDateForTimezone(date, timezone);
-    return formatTime(date, {separator: ':'});
-  }
-}
 
 export function selectCity(city)
 {
