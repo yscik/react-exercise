3637ba38b9e9a7dd373d15ef044edb5da2cb7da9
Peter Kiss <peter@yscik.com>
2019-10-28 19:28:36 +0100

    db initialization & migration

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,6 @@
 import pg from 'pg';
+import {promises as fs} from 'fs';
+import path from 'path';
 
 let client;
 
@@ -12,6 +14,7 @@ export async function createDbClient() {
 
 export async function initDb() {
   await createDbClient();
+  await executeMigrations();
 }
 
 export async function query(sql, params = []) {
@@ -23,3 +26,66 @@ export async function querySingle(sql, params = []) {
   const {rowCount,rows} = await query(sql, params);
   return rowCount ? rows[0] : null;
 }
+
+async function executeMigrations()
+{
+  const files = await fs.readdir('migrations');
+  const scripts = files.map(filename => ({
+    id: +filename.slice(0, filename.indexOf('-')),
+    filename: `./migrations/${filename}`,
+    type: filename.slice(filename.lastIndexOf('.')+1)
+  }));
+  scripts.sort((a,b) => a.id - b.id);
+
+  const version = await getVersion();
+  console.log('[DB] At version', version);
+
+  let pendingScripts = scripts.filter(s => s.id > version);
+
+  for(let script of pendingScripts) {
+    await executeMigration(script);
+  }
+
+}
+
+async function executeMigration(script)
+{
+  switch(script.type) {
+    case 'js':
+      const scriptFn = await import(script.filename);
+      await scriptFn.default();
+      break;
+    case 'sql':
+      const sql = await fs.readFile(script.filename, {encoding: 'utf8'});
+      await query(sql);
+      break;
+  }
+
+  await setVersion(script.id);
+}
+
+async function getVersion() {
+  const {rowCount: dbExists} = await query(`
+    SELECT 1
+    FROM information_schema.tables
+    WHERE table_schema = current_schema()
+    AND table_name = 'app'
+  `);
+
+  if (!dbExists)
+    return 0;
+
+  const {value} = await querySingle(`
+    SELECT value from app WHERE key = 'version'
+  `);
+
+  return +value;
+}
+
+async function setVersion(version) {
+  return query(`
+    UPDATE app SET value = $1 WHERE key = 'version'
+  `, [version]);
+}
+
+
